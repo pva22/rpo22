f9a5ce6877667f2fa5bc5060076683b41a66d198
Plotnikov Vlad <pva484@mail.ru>
2022-05-03 23:33:19 +0300

    LR8 done

---
+++ backend/front/src/components/Another_Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+class Another_Home extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="mt-5 me-auto">
+                <h2>Лабораторная работа № 8 по РПО</h2>
+
+                <div className="mt-15 me-auto">
+                    <p>ИУ3 2022 год</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Another_Home;
